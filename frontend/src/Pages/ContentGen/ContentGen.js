7114362d5c9ea92c40e9297b7eaa8fa237b3ea12
noname <nomail@nomail.com>
2025-04-22 10:58:03 +0530

    Local changes before origin pull and merge

--- frontend/src/Pages/ContentGen/ContentGen.js
+++ frontend/src/Pages/ContentGen/ContentGen.js
@@ -18,7 +18,7 @@ const ContentGen = () => {
     // Debugging log to verify data being sent
     console.log("Sending form data to backend:", formData);
 
-    fetch("http://127.0.0.1:5000/generate", {
+    fetch("http://127.0.0.1:5001/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
